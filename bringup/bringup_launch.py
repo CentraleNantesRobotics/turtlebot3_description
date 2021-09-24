2e9fba38073b0aaef7b49cd7e6bc2fcf13d27ae5
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2021-09-24 16:58:18 +0200

    tested and rolling

--- bringup/bringup_launch.py
+++ bringup/bringup_launch.py
@@ -28,7 +28,8 @@ def generate_launch_description():
     sl = SimpleLauncher()
     
     # all happens in this namespace
-    sl.declare_arg('name', os.environ['HOSTNAME'])
+    
+    sl.declare_arg('name', os.uname().nodename)
     
     sl.declare_arg('usb_port', default_value='/dev/ttyACM0', description='Connected USB port with OpenCR')
     
@@ -42,12 +43,22 @@ def generate_launch_description():
     with sl.group(ns=sl.arg('name')):
         
         sl.robot_state_publisher('turtlebot3_description','turtlebot3_' + TURTLEBOT3_MODEL + '.urdf.xacro', 
-                                 xacro_args={'prefix': sl.name_joint(sl.arg('name'), '/')},
+                                 xacro_args={'prefix': sl.name_join(sl.arg('name'), '/')},
                                  parameters={'use_sim_time': sl.arg('use_sim_time')})
         
         sl.include('hls_lfcd_lds_driver', 'hlds_laser.launch.py', 
-                   launch_arguments={'port': '/dev/ttyUSB0', 'frame_id': sl.name_joint(sl.arg('name'), 'base_scan')})
+                   launch_arguments={'port': '/dev/ttyUSB0', 'frame_id': sl.name_join(sl.arg('name'), '/base_scan')})        
+        
+        configured_params = RewrittenYaml(
+                            source_file=sl.arg('tb3_param_dir'),
+                            root_key=sl.arg('name'),
+                            param_rewrites={'frame_id': sl.name_join(sl.arg('name'), '/odom'),
+                                            'child_frame_id': sl.name_join(sl.arg('name'), '/base_footprint')},
+                            convert_types=True)
+                
         
         sl.node('turtlebot3_node', 'turtlebot3_ros',
-                parameters=[sl.arg('tb3_param_dir')],
+                parameters=[configured_params],
                 arguments=['-i', sl.arg('usb_port')])
+
+    return sl.launch_description()
